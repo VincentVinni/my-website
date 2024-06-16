2a75cefda2d90159ae28e98b16955a86c0ab25bc
Vincent <aungmoemyint@berkeley.edu>
2024-06-16 02:11:45 -0700

    fixed education

--- my-react-app/src/Componenets/greybox.js
+++ my-react-app/src/Componenets/greybox.js
@@ -6,7 +6,7 @@ const GREYBOX = () => {
         <div className="greybox-container">
             <p className='header'>Hi my name is Vincent!</p>
             <p className="greybox-content">
-                I'm a fourth year Computer Science student at UC Berkeley. 
+                I'm a fourth year Computer Science and Data Science student at UC Berkeley. 
                 I'm an avid learner and problem solver who enjoys the intersection of artificial intelligence, machine learning, and software development with 
                 real life entrepreneurship. In my spare time you will most likely find me in the gym, playing chess or catching up on a good 
                 Netflix thriller series!
